0fe2d40d40b3c060462fcc57927fdf8571ffa5f0
xhad <xhad@users.noreply.github.com>
2015-11-05 10:16:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -167,36 +167,6 @@ apiRoutes.get('/tutorials', function(req, res) {
 app.use('/api', apiRoutes);
 
 
-// -------------------------------
-// from MEAN CRUD
-// ------------------------------
-// app.use(methodOverride('X-HTTP-Method-Override'));
-
-// // CORS Support
-// app.use(function(req, res, next) {
-//    res.header('Access-Control-Allow-Origin', 'http://localhost');
-//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type');
-//    next();
-// });
-
-// app.use('/hello', function(req, res, next) {
-//    res.send('Hello World!');
-//    next();
-// });
-
-// // // Connect to MongoDB
-
-// // mongoose.connection.once('open', function() {
-
-// //    app.models = require('./models/index');
-// //    // Load the routes.
-// //    var routes = require('./routes');
-// //    _.each(routes, function(controller, route) {
-// //       app.use(route, controller(app, route));
-// //    });
-
-
 // ---------------------------------
 // Run the Server
 // ---------------------------------
